8e0c96e6218a3f18a124079a21dac0e735fa2882
lzhabo <52199564+lzhabo@users.noreply.github.com>
2025-09-24 17:20:06 +0300

    Updates

--- assets/all-wallets-Cnsax_i-.js
+++
@@ -1,6 +0,0 @@
-import{b as o}from"./core-BZXXvjDG.js";import"./index-txgEeEVz.js";import"./index.es-DD5hAw04.js";import"./index-nibyPLVP.js";import"./fallback-BHId7jGn.js";const p=o`<svg fill="none" viewBox="0 0 24 24">
-  <path
-    style="fill: var(--wui-color-accent-100);"
-    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
-  />
-</svg>`;export{p as allWalletsSvg};
