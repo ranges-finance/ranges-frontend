2402b22ab685580da04f6065fce311a5a57c4ba2
lzhabo <52199564+lzhabo@users.noreply.github.com>
2025-09-24 16:10:36 +0300

    Updates

---
+++ assets/cursor-CC9k3Gr3.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-BZXXvjDG.js";import"./index-txgEeEVz.js";import"./index.es-DD5hAw04.js";import"./index-nibyPLVP.js";import"./fallback-BHId7jGn.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
